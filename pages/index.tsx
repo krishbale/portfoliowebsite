79b6f2e583c360996823a5fff32e069f9dcc245c
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-05-04 21:13:18 +0545

    portfolio changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,32 @@
 import Head from 'next/head'
-import { BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillTwitterCircle,AiFillLinkedin,AiFillYoutube} from 'react-icons/ai';
+import { BsFillMoonStarsFill, BsMedium} from 'react-icons/bs';
+import {FaBloggerB} from 'react-icons/fa';
+import {AiFillGithub,AiFillLinkedin,AiFillYoutube} from 'react-icons/ai';
 import Image from 'next/image';
-import deved from '../public/dev-ed-wave.png';
+
 import design from '../public/design.png';
 import code from '../public/code.png';
 import consulting from '../public/consulting.png';
-import web1 from '../public/web1.png';
-import web2 from '../public/web2.png';
-import web3 from '../public/web3.png';
-import web4 from '../public/web4.png';
-import web5 from '../public/web5.png';
-import web6 from '../public/web6.png';
+import ecom1 from '../public/ecom1.png';
+import ecom2 from '../public/ecom2.png';
+import ecom3 from '../public/ecom3.png';
+import ecom4 from '../public/ecom4.png';
+import ecom5 from '../public/ecom5.png';
+import ecom6 from '../public/ecom6.png';
+
+import ecom7 from '../public/ecom7.png';
+import ecom8 from '../public/ecom8.png';
+import ecom9 from '../public/ecom9.png';
+import ecom10 from '../public/ecom10.png';
+import ecom11 from '../public/ecom11.png';
+import ecom12 from '../public/ecom12.png';
+import ecom13 from '../public/ecom13.png';
+import ecom14 from '../public/ecom14.png';
+
+
 import profile from '../public/profile.png'
 import {useState } from 'react';
+import Link from 'next/link';
 
 
 export default function Home() {
@@ -40,14 +53,21 @@ export default function Home() {
           </nav>
           <div className=' text-center p-10 dark:text-white '>
             <h2 className=' text-5xl py-2 text-teal-600  font-medium md:text-6xl'>Balkrishna Pokharel</h2>
-            <h3 className='text-2xl py-2 md:text-3xl'>FULL STACK Developers</h3>
+            <h3 className='text-2xl py-2 md:text-3xl'>React  Developer</h3>
             <p className='text-md py-3 leading-8 text-gray-800 md:text-lg max-w- mx-auto dark:text-white'>CS & IT Students</p>
 
           </div>
           <div className='text-5xl flex justify-center gap-16 text-gray-600 dark:text-white'>
-          <AiFillTwitterCircle />
-          <AiFillLinkedin />
-          <AiFillYoutube />
+            <Link  href='https://github.com/krishbale' > <AiFillGithub />
+            </Link>
+         <Link href={"https://www.linkedin.com/in/balkrishna-pokharel-600a4814a/"} >          <AiFillLinkedin  />
+
+         </Link>
+          <Link  href='https://medium.com/@sushil.pokharel.7528'>
+          <BsMedium />
+          </Link>
+         
+        
           </div>
           <div className='relative mx-auto bg-gradient-to-t from-teal-500 rounded-full w-80 h-80 mt-10 overflow-hidden md:h-96 md:w-96 ' >
             <Image src={profile}  alt=''   />
@@ -56,8 +76,14 @@ export default function Home() {
         <section>
           <div className='dark:text-white'>
             <h3 className='text-3xl py-1 leading-8 text-gray-800 dark:text-white'>About me</h3>
-            <p>Balkrishna Pokharel versatile web developers and programmers due to his extensive history in <span className='text-teal-500'>programming</span> and web design. His dedication to continuously learning about new web design trends makes him the beneficial  and valuable for any company.   </p>
-            <p>With 4 years of<span className='text-teal-500'> experience </span> and a studing bachlor 's in Computer Science & IT , his learning can help customers modernize with websites and appeal to expanding customer expectations.</p>
+                          <p> <span className='text-teal-500'>Passionate</span> Web Developer with a total of 1 year of experience,
+              proficient in designing and developing Web Application.<span className='text-teal-500'>Hard
+              working</span>  and  passionate person who is looking for a company with
+              an open environment and a great culture where he can turn his
+              knowledge into meaningful work while serving the company.
+              </p>
+            <p>With 4 years of knowledge in Bachlor Degree of CS & IT and is <span className='text-teal-500'> dedication </span>
+            to continuously learning about new web design trends makes him the beneficial and valuable for any company. </p>
           </div>
           <div className='lg:flex gap-10 dark:text-white'>
             <div className='text-center shadow-lg p-10 rounded-xl  my-10  '>
@@ -65,9 +91,9 @@ export default function Home() {
               <h3 className='text-lg font-medium pt-8 pb-2'>Designing</h3>
               <p className='py-2'>Creating elegant design for your needs and requirements specially using for web development</p>
               <h4 className='py-4 text-teal-600'>Design Tools I use</h4>
-              <p className='text-gray-800 py-1 dark:text-white '>Photoshop</p>
-              <p className='text-gray-800 py-1  dark:text-white '>Illustrator</p>
-              <p className='text-gray-800 py-1  dark:text-white '>Figma</p>
+              <p className='text-gray-800 py-1 dark:text-white '>Figma</p>
+              <p className='text-gray-800 py-1  dark:text-white '>Canava</p>
+              <p className='text-gray-800 py-1  dark:text-white '>Illustrators</p>
 
 
             </div >
@@ -76,9 +102,9 @@ export default function Home() {
               <h3 className='text-lg font-medium pt-8 pb-2'>Coding</h3>
               <p className='py-2'> Clean  Coding   with responsive design and industry standard  </p>
               <h4 className='py-4 text-teal-600'>Main Coding Technology/Platform  </h4>
-              <p className='text-gray-800 py-1  dark:text-white '>React Redux </p>
-              <p className='text-gray-800 py-1  dark:text-white '>Express Node  </p>
-              <p className='text-gray-800 py-1  dark:text-white '>MongoDB  Firebase</p>
+              <p className='text-gray-800 py-1  dark:text-white '>HTML ,CSS, JS, React, Bootstrap,Tail-Wind </p>
+              <p className='text-gray-800 py-1  dark:text-white '>Node JS , Express JS , Prisma </p>
+              <p className='text-gray-800 py-1  dark:text-white '>Mongo DB(Moongoose)   </p>
 
 
             </div>
@@ -86,10 +112,10 @@ export default function Home() {
               <Image className='mx-auto'  src={consulting} width="100" height={100} alt=""/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Testing </h3>
               <p className='py-2'>Strong Debugging skills , and problems solving ideas and skills  </p>
-              <h4 className='py-4 text-teal-600'>Testing and deployments</h4>
-              <p className='text-gray-800 py-1  dark:text-white '>Bug Testing</p>
-              <p className='text-gray-800 py-1 dark:text-white'  >Deployments </p>
-              <p className='text-gray-800 py-1  dark:text-white '>Maintainance</p>
+              <h4 className='py-4 text-teal-600'>Testing and Optimization</h4>
+              <p className='text-gray-800 py-1  dark:text-white '>Refactoring</p>
+              <p className='text-gray-800 py-1 dark:text-white'  >API Testing Using PostMan </p>
+              <p className='text-gray-800 py-1  dark:text-white '>Manual Software Testing </p>
 
 
             </div>
@@ -98,21 +124,44 @@ export default function Home() {
         <section>
           <div className='dark:text-white'>
             <h3 className='text-3xl py-1'>Porfolio</h3>
-            <p> Projects  completed in <span className='text-teal-500'>Web Development</span> and <span className='text-teal-500'> Designing. </span></p>
+            <p> You can find the project details in  <Link href={"https://github.com/krishbale"}><span  className='text-teal-500'>Github.</span></Link> Some of the project  snippets completed in <span className='text-teal-500'>Web Developement </span>are given below:-</p>
             <p></p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={web1} alt="" /></div>
           
-            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={web2} alt="" /></div>
+            <Link href={"https://nextflixdeploy-4hmt.vercel.app/auth"}>
+            
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom4} alt="" /></div>
           
-            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={web3} alt="" /></div>
+            {/* <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom6} alt="" /></div> */}
+            {/* <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom5} alt="" /></div> */}
+            {/* <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom7} alt="" /></div> */}
+            </Link>
+            {/* <Link href={"https://github.com/krishbale/reduxtoolkitusertable"}>
+            
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom8} alt="" /></div>
+            </Link> */}
+            {/* <Link href={"https://github.com/krishbale/Travel-Log-App"}>
           
-            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={web4} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom9} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom10} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom11} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom12} alt="" /></div>
+            </Link> */}
+            {/* <Link href={"https://github.com/krishbale/Chatapp-with-socket-io-and-node-js"}>
+    
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom13} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom14} alt="" /></div>
+
+            </Link> */}
+            {/* <Link href={"https://github.com/krishbale/final_year_projects"}>
+            
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom1} alt="" /></div>
           
-            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={web5} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom2} alt="" /></div>
           
-            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={web6} alt="" /></div>
+            <div className=' basis-1/3 flex-1 '><Image className=' rounded-lg object-cover w-100% h-100%' layout='responsive'  src={ecom3} alt="" /></div>
+            </Link> */}
             </div>
         </section>
 
