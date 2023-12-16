6a544c830cf3056a6dc3451d8d67ceeb8cf34d57
krishbale <sushil.pokharel.7528@gmail.com>
2023-12-16 19:56:39 +0545

    updated dark

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 import { useState } from "react";
 import Hero from "@/components/Hero";
 export default function Home() {
-  const [darkmode, setDarkmode] = useState(false);
+  const [darkmode, setDarkmode] = useState(true);
   return (
     <>
       <div className={darkmode ? "dark" : ""}>
